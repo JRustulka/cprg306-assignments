9f16b3e92a87d747e3c5d6171e85868d226c0b38
JRustulka <jrustulka@gmail.com>
2024-09-18 13:24:28 -0600

    Week-3 Assignment fixes

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,6 +1,6 @@
 export default function Item({ name, quantity, category }) {
   return (
-    <li className="bg-slate-900 m-4 w-96 p-3">
+    <li className="bg-slate-900 text-white m-4 w-96 p-3 border-white border-2">
       <h2 className="font-bold text-xl">{name}</h2>
       <p>
         Buy {quantity} in {category}
